295422aca2e56ef000f99e3a42c5005fae7e0ce0
ilgo123 <prayogoilham27@gmail.com>
2025-02-20 20:35:26 +0700

    praktikum 01

---
+++ variable_konstanta.php
@@ -0,0 +1,8 @@
+<?php
+define('PHI',3.14);
+
+$jari = 100;
+$luas = PHI * $jari * $jari;
+
+echo 'Luas Lingkaran dengan jari-jari ' . $jari . ' cm adalah ' . $luas . ' cm²';
+?>
---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+$nama = 'Ilham Prayogo';
+$umur = 21;
+$berat = 55.5;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur . ' Tahun';
+echo '<br/>Berat : ' . $berat . ' Kg';
+
+echo "<br/>Hello $nama Apa kabar?";
+?>
